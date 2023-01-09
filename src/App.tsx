4ec35d7e6adeedef19c25f00bdeaa3ebcdeb09dc
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2023-01-09 19:18:20 -0400

    Adding Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import type { FC } from "react";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./Pages";
+
 const App: FC = (): JSX.Element => {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
